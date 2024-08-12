00059df2e0746ea3f30c582924188abe0e6842be
julibucci <buccijulieta0@gmail.com>
2024-08-12 16:38:42 -0300

    create the class Vehicle, which is the superclass of the class hierarchy

---
+++ src/model/Vehicle.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Vehicle
+{
+    // Attributes
+    private String brand;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getter y setter
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // Start method
+    public void start() {
+        System.out.println("The vehicle is starting.");
+    }
+
+}
